e533eb86678abec3a91e7dfdc41a663cab7ec545
Michael Mboya <mikemboya2@gmail.com>
2025-07-13 08:10:30 +0530

    lints

--- twitter-clone/src/lib/firebase/config.ts
+++ twitter-clone/src/lib/firebase/config.ts
@@ -4,7 +4,7 @@ const config = {
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 } as const;
 
 type Config = typeof config;
