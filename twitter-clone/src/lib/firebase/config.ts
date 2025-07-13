707be815cbf0bbe462829f059ad0388239b2ebdf
Michael Mboya <mikemboya2@gmail.com>
2025-07-13 07:40:17 +0530

    remove optional measurement id

--- twitter-clone/src/lib/firebase/config.ts
+++ twitter-clone/src/lib/firebase/config.ts
@@ -5,7 +5,6 @@ const config = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 } as const;
 
 type Config = typeof config;
